28249564e7b4c7889543f3a1bbe871834e3a9f62
tausif71 <91406350+tausif71@users.noreply.github.com>
2021-10-28 13:49:32 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -10,3 +10,12 @@ firebase.initializeApp({
     measurementId: "G-HTR85R42RM"
 });
 const messaging = firebase.messaging();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+      .then(function(registration) {
+        console.log('Registration successful, scope is:', registration.scope);
+      }).catch(function(err) {
+        console.log('Service worker registration failed, error:', err);
+      });
+    }
