6743cc6f727b0355151915cdbd2268e2241617c7
tausif71 <91406350+tausif71@users.noreply.github.com>
2021-10-28 14:19:45 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-messaging.js');
-firebase.initializeApp({
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
     apiKey: "AIzaSyAc5UTq8YcCblJFyQzJTl6wrjkVSCsBuEs",
     authDomain: "attendancepwa-e8305.firebaseapp.com",
     projectId: "attendancepwa-e8305",
@@ -8,6 +14,8 @@ firebase.initializeApp({
     messagingSenderId: "751666883415",
     appId: "1:751666883415:web:82cbb0080bc6a3f30a6c12",
     measurementId: "G-HTR85R42RM"
-});
-const messaging = firebase.messaging();
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
