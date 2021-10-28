09fcd7735de12a09f7896f223741133044b97c20
tausif71 <91406350+tausif71@users.noreply.github.com>
2021-10-28 21:49:20 +0530

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.7.0/firebase-messaging.js');
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
     apiKey: "AIzaSyAc5UTq8YcCblJFyQzJTl6wrjkVSCsBuEs",
     authDomain: "attendancepwa-e8305.firebaseapp.com",
     projectId: "attendancepwa-e8305",
@@ -14,8 +15,8 @@ const firebaseConfig = {
     messagingSenderId: "751666883415",
     appId: "1:751666883415:web:82cbb0080bc6a3f30a6c12",
     measurementId: "G-HTR85R42RM"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
